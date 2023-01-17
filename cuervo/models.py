83819e38435fd380c5ce52347d508df92f33a8c0
Ivan Alejandro Castillo Flores <alex9012345678@gmail.com>
2023-01-17 14:07:45 -0600

    models

--- cuervo/models.py
+++ cuervo/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.urls import reverse
+from django.conf import settings
 
 # Create your models here.
 class inventoryLocation(models.Model):
@@ -26,7 +26,7 @@ class coil(models.Model):
     FK_coilType_id = models.ForeignKey(coilType, on_delete=models.CASCADE, null=False, help_text='Linked Coil Type')
     FK_coilProvider_id = models.ForeignKey(coilProvider, on_delete=models.CASCADE, null=False, help_text='Linked Coil Provider')
     last_update = models.DateTimeField(auto_now_add=True, null=False)
-    last_edit_user = models.DateTimeField(auto_now_add=False, null=True)
+    last_edit_user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, null=False, help_text='Linked User' )
 
 class label(models.Model):
     uniqueid = models.CharField(max_length=20)
@@ -34,7 +34,22 @@ class label(models.Model):
     FK_labelStatus_id = models.ForeignKey(labelStatus, on_delete=models.CASCADE, null=False, help_text='Linked Label Status')
     FK_inventoryLocation_id = models.ForeignKey(inventoryLocation, on_delete=models.CASCADE, null=False, help_text='Linked Inventory Location')
     last_update = models.DateTimeField(auto_now_add=True, null=False)
-    last_edit_user = models.DateTimeField(auto_now_add=False, null=True)
+    last_edit_user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, null=False, help_text='Linked User')
+
+class labelTrace(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True, null=False)
+    FK_label_id = models.ForeignKey(label,on_delete=models.CASCADE, null=False, help_text='Linked Label')
+    FK_labelStatus_id = models.ForeignKey(labelStatus, on_delete=models.CASCADE, null=False, help_text='Linked Label Status')
+    FK_inventoryLocation_id = models.ForeignKey(inventoryLocation, on_delete=models.CASCADE, null=False, help_text='Linked Inventory Location')
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, help_text='Linked User')
+
+class coilTrace(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True, null=False)
+    FK_coil_id = models.ForeignKey(coil, on_delete=models.CASCADE, null=False, help_text='Linked Coil')
+    FK_coilStatus_id = models.ForeignKey(coilStatus, on_delete=models.CASCADE, null=False, help_text='Linked Coil Status')
+    FK_coilType_id = models.ForeignKey(coilType, on_delete=models.CASCADE, null=False, help_text='Linked Coil Type')
+    FK_coilProvider_id = models.ForeignKey(coilProvider, on_delete=models.CASCADE, null=False, help_text='Linked Coil Provider')
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, help_text='Linked User')
 
 
 
